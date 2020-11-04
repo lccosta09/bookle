9da259244bf8179644252af6b8f4230c7af2186a
lcorreia <correialesp@gmail.com>
2020-11-04 19:29:08 -0300

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div id="nav">
-      <router-link to="/">Hello World</router-link>
-      <router-link to="/register">Registre-se</router-link>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <div id="nav">
+            <router-link to="/">Hello World</router-link>
+            <router-link to="/register">Registre-se</router-link>
+        </div>
+        <router-view />
     </div>
-    <router-view />
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App'
 }
 </script>
 
