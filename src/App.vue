ba2e496eac6cd4733b3e71dade5079270887f1cb
lcorreia <correialesp@gmail.com>
2020-11-04 23:24:08 -0300

    navegacao

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <template>
     <div>
-        <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
-            <div class="container">
-                <router-link :to="{ name: 'home' }" class="navbar-brand">Bookle</router-link>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'register' }" class="nav-link">Registre-se</router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <NavBar />
         <div class="container">
             <router-view />
         </div>
@@ -19,8 +8,13 @@
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        NavBar
+    }
 }
 </script>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+            <div class="container">
+                <router-link :to="{ name: 'home' }" class="navbar-brand">Bookle</router-link>
+                <button class="navbar-toggler" :class="{'collapsed': collapsed}" type="button" v-on:click="toogleCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" :class="{'collapse': collapsed, 'show': !collapsed}" id="navbarResponsive">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'register' }" class="nav-link">Registre-se</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            collapsed: true
+        }
+    },
+    methods: {
+        toogleCollapse() {
+            this.collapsed = !this.collapsed; 
+        }
+    }
+}
+</script>
+
+<style>
+</style>
