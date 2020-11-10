e051c9efb29a10cf2956583fbcdb23f9a81c0e38
lcorreia <correialesp@gmail.com>
2020-11-10 20:56:00 -0300

    doctors

--- src/components/Doctors.vue
+++ src/components/Doctors.vue
@@ -1,16 +1,12 @@
 <template>
-    <div>
-        <div class="row">
-            <div v-for="doctor in this.$store.state.doctor.doctors" :key="doctor.id">
-                <div class="col-lg-4" style="max-width: 20rem;">
-                    <div class="bs-component">
-                        <div class="card border-primary mb-3">
-                            <div class="card-header" v-text="doctor.name"></div>
-                            <div class="card-body">
-                                <h4 class="card-title">Primary card title</h4>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
+    <div class="row">
+        <div v-for="doctor in this.$store.state.doctor.doctors" :key="doctor.id" class="col-lg-4">
+            <div class="bs-component">
+                <div class="card border-primary mb-3">
+                    <div class="card-header" v-text="doctor.name"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Primary card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
             </div>
@@ -20,9 +16,7 @@
 
 <script>
 export default {
-    name: 'Doctors',
-    data() {
-    }
+    name: 'Doctors'
 }
 </script>
 
