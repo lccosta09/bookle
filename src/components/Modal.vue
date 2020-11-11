dd82bace70282736fb3071069c60de4deff49ff2
lcorreia <correialesp@gmail.com>
2020-11-11 00:28:31 -0300

    modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,20 +1,18 @@
 <template>
     <div>
-        <div class="modal fade show" v-show="isModalOpen">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Modal title</h5>
-                        <button type="button" class="close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Modal body text goes here.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" v-on:click="$emit('close-modal')">Close</button>
-                    </div>
+        <div class="modal-dialog" show v-show="isModalOpen">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" v-on:click="$emit('close-modal')">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" v-on:click="$emit('close-modal')">Close</button>
                 </div>
             </div>
         </div>
@@ -40,5 +38,17 @@ export default {
 <style scoped>
     .show {
         display: block;
+    }
+
+    .modal-backdrop {
+        position: absolute;
+        z-index: 2000;
+    }
+
+    .modal-dialog {
+        position: absolute;
+        z-index: 2001;
+        top: 0;
+        left: 0;
     }
 </style>
