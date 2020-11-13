894813c7c417307234b97ac7c701eebfe82eaaa3
lcorreia <correialesp@gmail.com>
2020-11-13 00:55:17 -0300

    calendar

--- src/components/Doctor.vue
+++ src/components/Doctor.vue
@@ -3,119 +3,9 @@
         <p>{{ doctor.description }}</p>
         <div class="calendar-wrapper">
             <div class="calendar">
-                <ul class="pagination pagination-lg">
-                    <li class="page-item">
-                        <button class="page-link">1</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">2</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">3</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">4</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">5</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">5</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">7</button>
-                    </li>
-                </ul>
-                <ul class="pagination pagination-lg">
-                    <li class="page-item">
-                        <button class="page-link">8</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">9</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">10</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">11</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">12</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">13</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">14</button>
-                    </li>
-                </ul>
-                <ul class="pagination pagination-lg">
-                    <li class="page-item">
-                        <button class="page-link">15</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">16</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">17</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">18</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">19</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">20</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">21</button>
-                    </li>
-                </ul>
-                <ul class="pagination pagination-lg">
-                    <li class="page-item">
-                        <button class="page-link">22</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">23</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">24</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">25</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">26</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">27</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">28</button>
-                    </li>
-                </ul>
-                <ul class="pagination pagination-lg">
-                    <li class="page-item">
-                        <button class="page-link">29</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">30</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link">31</button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link"></button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link"></button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link"></button>
-                    </li>
-                    <li class="page-item">
-                        <button class="page-link"></button>
+                <ul class="pagination pagination-lg" v-for="(week, x) in month" :key="`week-${x}`">
+                    <li class="page-item" v-for="(day, y) in week" :key="`day-${y}`">
+                        <button class="page-link">{{ day }}</button>
                     </li>
                 </ul>
             </div>
@@ -128,7 +18,53 @@ export default {
     name: 'Doctor',
     props: [
         'doctor'
-    ]
+    ],
+    data() {
+        return {
+            currentYear: 2021,
+            currentMonth: 0,
+            month: [
+                [1, 2, 3, 4, 5, 6, 7],
+                [8, 9, 10, 11, 12, 13, 14],
+                [15, 16, 17, 18, 19, 20, 21],
+                [22, 23, 24, 25, 26, 27, 28],
+                [29, 30, 31]
+            ]
+        }
+    },
+    mounted() {
+        const firstDay = new Date(this.currentYear, this.currentMonth, 1);
+        let lastDay = new Date(this.currentYear, this.currentMonth, 1);
+        lastDay.setMonth(lastDay.getMonth() + 1);
+        lastDay.setDate(lastDay.getDate() - 1);
+        const dayOfWeek = firstDay.getDay();
+        this.month = [];
+
+        let day = 1;
+        for (let i = 0; i < 6; i++) {
+            let week = [];
+            for (let j = 0; j < 7; j++) {
+                if (i === 0 && j < dayOfWeek) {
+                    week = [...week, ''];
+                    continue;
+                }
+
+                if (day > lastDay.getDate()) {
+                    week = [...week, ''];
+                    continue;
+                }
+
+                week = [...week, day];
+                day++;
+            }
+
+            this.month = [...this.month, week];
+
+            if (day > lastDay.getDate()) {
+                break;
+            }
+        }
+    }
 };
 </script>
 
