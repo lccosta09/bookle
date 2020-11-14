997442a4bb29219c5dd24f90b853feb357fa564c
lcorreia <correialesp@gmail.com>
2020-11-13 22:00:55 -0300

    calendar

--- src/components/Doctor.vue
+++ src/components/Doctor.vue
@@ -1,87 +1,20 @@
 <template>
     <div>
         <p>{{ doctor.description }}</p>
-        <div class="calendar-wrapper">
-            <div class="calendar">
-                <ul class="pagination pagination-lg">
-                    <li class="page-item"><button class="page-link">D</button></li>
-                    <li class="page-item"><button class="page-link">S</button></li>
-                    <li class="page-item"><button class="page-link">T</button></li>
-                    <li class="page-item"><button class="page-link">Q</button></li>
-                    <li class="page-item"><button class="page-link">Q</button></li>
-                    <li class="page-item"><button class="page-link">S</button></li>
-                    <li class="page-item"><button class="page-link">S</button></li>
-                </ul>
-                <ul class="pagination pagination-lg" v-for="(week, x) in month" :key="`week-${x}`">
-                    <li class="page-item" v-for="(day, y) in week" :key="`day-${y}`">
-                        <button class="page-link">{{ day.text }}</button>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <Calendar />
     </div>
 </template>
 
 <script>
+import Calendar from './Calendar.vue';
+
 export default {
     name: 'Doctor',
     props: [
         'doctor'
     ],
-    data() {
-        return {
-            currentYear: 2020,
-            currentMonth: 11,
-            month: []
-        }
-    },
-    mounted() {
-        let date = new Date(this.currentYear, this.currentMonth, 1);
-        let lastDate = new Date(this.currentYear, this.currentMonth, 1);
-        lastDate.setMonth(lastDate.getMonth() + 1);
-        lastDate.setDate(lastDate.getDate() - 1);
-        date.setDate(date.getDate() - date.getDay());
-        this.month = [];
-
-        for (let i = 0; i < 6; i++) {
-            let week = [];
-            for (let j = 0; j < 7; j++) {
-                week = [...week, {
-                    text: date.getDate()
-                }];
-
-                date.setDate(date.getDate() + 1);
-            }
-
-            this.month = [...this.month, week];
-
-            if (date.getMonth() > lastDate.getMonth() || date.getYear() > lastDate.getYear()) {
-                break;
-            }
-        }
+    components: {
+        Calendar
     }
 };
 </script>
-
-<style scoped>
-    .calendar-wrapper {
-        text-align: center;
-    }
-
-    .calendar {
-        text-align: left;
-        display: inline-block;
-    }
-
-    .pagination-lg .page-link {
-        padding: 0;
-        width: 50px;
-        height: 50px;
-        border: none;
-        border-radius: 0;
-    }
-
-    .pagination-lg {
-        margin: 0;
-    }
-</style>
