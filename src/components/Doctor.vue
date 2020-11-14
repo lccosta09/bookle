b25d3dd37d62dad1c15bb56f32147e807b48c6d6
lcorreia <correialesp@gmail.com>
2020-11-14 14:42:24 -0300

    schedule

--- src/components/Doctor.vue
+++ src/components/Doctor.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p>{{ doctor.description }}</p>
-        <Calendar v-on:choose-date="dateChosen" />
+        <Calendar v-if="step === CALENDAR" v-on:choose-date="showSchedule" />
     </div>
 </template>
 
@@ -13,12 +13,33 @@ export default {
     props: [
         'doctor'
     ],
+    data() {
+        return {
+            step: '',
+            schedule: []
+        }
+    },
     components: {
         Calendar
     },
+    created() {
+        this.CALENDAR = 'calendar';
+        this.SCHEDULE = 'schedule';
+        this.step = this.CALENDAR;
+    },
     methods: {
-        dateChosen(date) {
-            console.log(date);
+        showSchedule(date) {
+            // this.step = this.SCHEDULE;
+            this.schedule = this.getSchedule(date);
+        },
+        getSchedule(date) {
+            const schedules = JSON.parse(JSON.stringify(this.$store.state.schedule.schedules));
+
+            const selecteDate = new Date(date.year, date.month, date.day);
+            let [, doctorSchedules] = Object.entries(schedules).find(([doctorId,]) => this.doctor.id === parseInt(doctorId));
+            let [, dateSchedules] = Object.entries(doctorSchedules).find(([timestamp,]) => selecteDate.getTime() === parseInt(timestamp));
+
+            return dateSchedules;
         }
     }
 };
