9d1ef6bbc3f5132af802ae2e38c30444ea7bb877
lcorreia <correialesp@gmail.com>
2020-11-16 20:17:42 -0300

    schedule

--- src/store/modules/schedule.js
+++ src/store/modules/schedule.js
@@ -7,29 +7,35 @@ const schedule = {
                     1605492000000: [
                         {
                             start: '09:30',
-                            end: '10:00'
+                            end: '10:00',
+                            patientsLimit: 2
                         },
                         {
                             start: '14:00',
-                            end: '14:30'
+                            end: '14:30',
+                            patientsLimit: 2
                         },
                         {
                             start: '15:00',
-                            end: '15:30'
+                            end: '15:30',
+                            patientsLimit: 2
                         }
                     ],
                     1605578400000: [
                         {
                             start: '10:30',
-                            end: '11:00'
+                            end: '11:00',
+                            patientsLimit: 2
                         },
                         {
                             start: '11:00',
-                            end: '11:30'
+                            end: '11:30',
+                            patientsLimit: 2
                         },
                         {
                             start: '13:30',
-                            end: '14:00'
+                            end: '14:00',
+                            patientsLimit: 2
                         }
                     ]
                 }
