b30104f0b54f6f5a13d7c3d6f8b9ab699a1f4e05
lcorreia <correialesp@gmail.com>
2020-11-18 17:42:50 -0300

    appointments

--- src/components/Doctor.vue
+++ src/components/Doctor.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
+        <div v-if="page !== pages.CALENDAR" class="back" v-on:click="$emit('previous-page', page)">
+            <span class="icon">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                </svg>
+            </span>Voltar
+        </div>
+
         <Calendar
             v-if="page === pages.CALENDAR"
             :date="date"
-            v-on:set-date="onSetDate"
-            v-on:choose-date="onOpenSchedule" />
+            v-on:set-date="$emit('set-date', $event)"
+            v-on:choose-date="$emit('open-schedule', $event)" />
         <Schedules
             v-if="page === pages.SCHEDULE"
             :date="date"
             :schedule="dateSchedule"
-            v-on:previous-page="$emit('previous-page', page)"
-            v-on:book="onBook" />
+            v-on:book="$emit('book', $event)" />
     </div>
 </template>
 
@@ -30,17 +37,16 @@ export default {
     components: {
         Calendar,
         Schedules
-    },
-    methods: {
-        onSetDate(date) {
-            this.$emit('set-date', date);
-        },
-        onOpenSchedule(date) {
-            this.$emit('open-schedule', date);
-        },
-        onBook(interval) {
-            this.$emit('book', interval);
-        }
     }
 };
 </script>
+
+<style scoped>
+    .back {
+        height: 1.5rem;
+        line-height: 1.5rem;
+        cursor: pointer;
+        margin-bottom: 15px;
+        display: inline-block;
+    }
+</style>
--- src/components/Schedules.vue
+++ src/components/Schedules.vue
@@ -1,12 +1,5 @@
 <template>
     <div>
-        <div class="back" v-on:click="$emit('previous-page')">
-            <span class="icon">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-                </svg>
-            </span>Voltar
-        </div>
         <h5 class="date">{{ fullDate }}</h5>
         <div class="intervals">
             <div class="interval" v-for="(inteval, index) in schedule" :key="`inteval-${index}`">
@@ -69,13 +62,5 @@ export default {
     .intervals .interval .select {
         right: 0;
         position: absolute;
-    }
-
-    .back {
-        height: 1.5rem;
-        line-height: 1.5rem;
-        cursor: pointer;
-        margin-bottom: 15px;
-        display: inline-block;
     }
 </style>
