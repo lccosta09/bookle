a61b80cb380be45ce934cab631135437e76ca495
lcorreia <correialesp@gmail.com>
2020-11-19 23:32:40 -0300

    schedule

--- src/store/modules/schedule.js
+++ src/store/modules/schedule.js
@@ -89,6 +89,93 @@ const schedule = {
                             appointmentsLimit: 1
                         }
                     ]
+                },
+                2: {
+                    1605492000000: [
+                        {
+                            start: '09:30',
+                            end: '10:00',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '14:00',
+                            end: '14:30',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '15:00',
+                            end: '15:30',
+                            appointmentsLimit: 1
+                        }
+                    ],
+                    1605578400000: [
+                        {
+                            start: '10:30',
+                            end: '11:00',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '11:00',
+                            end: '11:30',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '13:30',
+                            end: '14:00',
+                            appointmentsLimit: 1
+                        }
+                    ],
+                    1606269600000: [
+                        {
+                            start: '09:30',
+                            end: '10:00',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '14:00',
+                            end: '14:30',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '15:00',
+                            end: '15:30',
+                            appointmentsLimit: 1
+                        }
+                    ],
+                    1606356000000: [
+                        {
+                            start: '10:30',
+                            end: '11:00',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '11:00',
+                            end: '11:30',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '13:30',
+                            end: '14:00',
+                            appointmentsLimit: 1
+                        }
+                    ],
+                    1606788000000: [
+                        {
+                            start: '11:30',
+                            end: '12:00',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '13:00',
+                            end: '14:30',
+                            appointmentsLimit: 1
+                        },
+                        {
+                            start: '15:30',
+                            end: '16:00',
+                            appointmentsLimit: 1
+                        }
+                    ]
                 }
             }
         }
