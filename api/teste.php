2abba19d2176d79cc1af6ef8749d27a10951b897
lcorreia <correialesp@gmail.com>
2020-11-21 16:15:59 -0300

    api

--- api/teste.php
+++ api/teste.php
@@ -1,2 +1,5 @@
 <?php
-echo 'oi';
+header('Content-Type: application/json');
+date_default_timezone_set('America/Sao_Paulo');
+$response = array('date' => strtotime(date('D M d Y 00:00:00 O')) . '000');
+echo json_encode($response);
