ab41d47ec712a3a871dd06ab9adf319e32c15011
lcorreia <correialesp@gmail.com>
2020-11-24 20:41:40 -0300

    api

--- api/book.php
+++ api/book.php
@@ -1,18 +1,1 @@
 <?php
-session_start();
-
-$appointments = [
-    'id' => 7,
-    'doctor' => 1,
-    'date' => '2020-11-25',
-    'start' => '09:30',
-    'end' => '10:00'
-];
-
-$input = file_get_contents('php://input');
-$input = json_decode($input, true);
-
-$doctorAppointments = array($appointments, function($appointments) use ($input) {
-    
-});
-
