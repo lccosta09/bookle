56cb1fa2e79d8fee95b2474fcbab115165fa5ce6
lcorreia <correialesp@gmail.com>
2020-11-24 22:22:00 -0300

    schedules

--- api/schedules.php
+++ api/schedules.php
@@ -13,29 +13,29 @@ if (!empty($_GET['doctorId'])) {
 }
 
 if (!empty($_GET['month'])) {
-    $conditions[] = "MONTH(start_hour) = {$_GET['month']}";
+    $conditions[] = "MONTH(starts_at) = {$_GET['month']}";
 }
 
 if (!empty($_GET['date'])) {
-    $conditions[] = "start_hour >= '{$_GET['date']} 00:00:00'";
-    $conditions[] = "start_hour <= '{$_GET['date']} 23:59:59'";
+    $conditions[] = "starts_at >= '{$_GET['date']} 00:00:00'";
+    $conditions[] = "starts_at <= '{$_GET['date']} 23:59:59'";
 }
 
-$conditions[] = !empty($_GET['year']) ? "YEAR(start_hour) = {$_GET['year']}" : 'YEAR(start_hour) = ' . date('Y');
+$conditions[] = !empty($_GET['year']) ? "YEAR(starts_at) = {$_GET['year']}" : 'YEAR(starts_at) = ' . date('Y');
 
 $conditions = implode(' AND ', $conditions);
 
-$sql = "SELECT doctors.id, schedules.start_hour, schedules.end_hour, schedules.appointments_limit
+$sql = "SELECT doctors.id, schedules.starts_at, schedules.ends_at, schedules.appointments_limit
     FROM schedules INNER JOIN doctors ON (schedules.doctor_id = doctors.id)
     WHERE $conditions";
 $data = $conn->query($sql);
 $response = [];
 foreach ($data as $schedule) {
-    $time = strtotime(date('D M d Y 00:00:00 O', strtotime($schedule['start_hour']))) . '000';
+    $time = strtotime(date('D M d Y 00:00:00 O', strtotime($schedule['starts_at']))) . '000';
     $response[$schedule['id']][$time][] = [
-        'start' => date('H:i', strtotime($schedule['start_hour'])),
-        'end' => date('H:i', strtotime($schedule['end_hour'])),
-        'date' => date('Y-m-d', strtotime($schedule['start_hour'])),
+        'start' => date('H:i', strtotime($schedule['starts_at'])),
+        'end' => date('H:i', strtotime($schedule['ends_at'])),
+        'date' => date('Y-m-d', strtotime($schedule['starts_at'])),
         'appointmentsLimit' => $schedule['appointments_limit']
     ];
 }
