2cf75abd638f9e2b3e34e7c7e10952e00f4ed218
lcorreia <correialesp@gmail.com>
2020-11-27 23:55:04 -0300

    headers

--- site/src/store/modules/appointment.js
+++ site/src/store/modules/appointment.js
@@ -21,76 +21,15 @@ const appointment = {
         }
     },
     actions: {
-        async getByDoctorAndDate({state}, payload) {
-            const appointments = JSON.parse(JSON.stringify(state.appointments));
-
-            const selectedDate = new Date(payload.date.year, payload.date.month, payload.date.day);
-            let entries = Object.entries(appointments).find(([doctorId,]) => payload.doctorId === parseInt(doctorId));
-            if (!entries) {
-                return [];
-            }
-
-            let [, doctorAppointments] = entries;
-            entries = Object.entries(doctorAppointments).find(([timestamp,]) => selectedDate.getTime() === parseInt(timestamp));
-            if (!entries) {
-                return [];
-            }
-
-            let [, dateAppointments] = entries;
-
-            return dateAppointments;
-        },
-        async getByDoctorAndMonth({state}, payload) {
-            const appointments = JSON.parse(JSON.stringify(state.appointments));
-
-            const selectedDate = new Date(payload.date.year, payload.date.month, payload.date.day);
-            let entries = Object.entries(appointments).find(([doctorId,]) => payload.doctorId === parseInt(doctorId));
-            if (!entries) {
-                return [];
-            }
-
-            let [, doctorAppointments] = entries;
-            entries = Object.entries(doctorAppointments).filter(([timestamp,]) => {
-                const date = new Date(parseInt(timestamp));
-                return selectedDate.getMonth() === date.getMonth() && selectedDate.getFullYear() === date.getFullYear();
-            });
-
-            if (!entries) {
-                return [];
-            }
-
-            return Object.fromEntries(entries);
-        },
-        async getByUserAndDoctor({state}, payload) {
-            const appointments = JSON.parse(JSON.stringify(state.appointments));
-
-            let userAppointments = [];
-
-            Object.entries(appointments).forEach(([doctorId, doctorAppointments]) => {
-                if (parseInt(doctorId) !== payload.doctorId) {
-                    return;
-                }
-
-                Object.entries(doctorAppointments).forEach(([timestamp, appointmens]) => {
-                    const date = new Date(timestamp);
-                    const today = new Date();
-
-                    if (date.getTime() < today.today) {
-                        return;
-                    }
-
-                    userAppointments = [...userAppointments, ...appointmens.filter((appointment) => {
-                        return parseInt(appointment.userId) === payload.userId;
-                    })];
-                });
-            });
-
-            return userAppointments;
-        },
-        async book({commit}, payload) {
+        async book({commit, rootState}, payload) {
             await axios.post('http://bookle-api.docker:1212/book.php', {
                     scheduleId: payload.scheduleId,
                     userId: payload.userId
+                },
+                {
+                    headers: {
+                        'Authorization': `${'Bearer'} ${rootState.user.loggedUser.token}`
+                    }
                 })
                 .then(response => {
                     commit('setLastAppointment', response.data);
--- site/src/store/modules/schedule.js
+++ site/src/store/modules/schedule.js
@@ -13,12 +13,16 @@ const schedule = {
         }
     },
     actions: {
-        async getByDoctorAndDate({commit}, payload) {
+        async getByDoctorAndDate({commit, rootState}, payload) {
             let month = payload.date.month + 1;
             const day = payload.date.day.toString().length < 2 ? '0'.repeat(2 - payload.date.day.toString().length) + payload.date.day : payload.date.day.toString();
             month = month.toString().length < 2 ? '0'.repeat(2 - month.toString().length) + month : month.toString();
 
+            axios.defaults.withCredentials = true;
             await axios.get('http://bookle-api.docker:1212/schedules.php', {
+                    headers: {
+                        'Authorization': `${'Bearer'} ${rootState.user.loggedUser.token}`
+                    },
                     params: {
                         doctorId: payload.doctorId,
                         year: payload.date.year,
@@ -30,10 +34,15 @@ const schedule = {
                     commit('setSchedule', response.data)
                 });
         },
-        async getByDoctorAndMonth({commit}, payload) {
+        async getByDoctorAndMonth({commit, rootState}, payload) {
             let month = payload.date.month + 1;
             month = month.toString().length < 2 ? '0'.repeat(2 - month.toString().length) + month : month.toString();
+
+            axios.defaults.withCredentials = true;
             await axios.get('http://bookle-api.docker:1212/schedules.php', {
+                    headers: {
+                        'Authorization': `${'Bearer'} ${rootState.user.loggedUser.token}`
+                    },
                     params: {
                         doctorId: payload.doctorId,
                         year: payload.date.year,
