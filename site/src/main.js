f839b05c3a39fca194815924641f75fa3dac8ebc
lcorreia <correialesp@gmail.com>
2020-11-30 22:41:44 -0300

    routes

--- site/src/main.js
+++ site/src/main.js
@@ -10,19 +10,3 @@ createApp(App)
     .use(router)
     .use(store)
     .mount('#app');
-
-router.beforeEach(async (to, from, next) => {
-    if (!store.state.user.loggedUser.token && to.name !== 'login') {
-        await store.dispatch('user/refreshToken', {}, {root: true});
-        if (store.state.user.loggedUser.token) {
-            next();
-        }
-
-        next({
-            path: 'login'
-        });
-        return;
-    }
-
-    next();
-});
--- site/src/router/index.js
+++ site/src/router/index.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 import Home from '../components/Home.vue';
 import Login from '../components/Login.vue';
 import Doctors from '../components/Doctors.vue';
+import store from '../store';
 
 const routes = [
     {
@@ -25,6 +26,22 @@ const router = createRouter({
     mode: 'history',
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach(async (to, from, next) => {
+    if (!store.state.user.loggedUser.token && to.name !== 'login') {
+        await store.dispatch('user/refreshToken', {}, {root: true});
+        if (store.state.user.loggedUser.token) {
+            next();
+        }
+
+        next({
+            path: 'login'
+        });
+        return;
+    }
+
+    next();
 });
 
 export default router;
