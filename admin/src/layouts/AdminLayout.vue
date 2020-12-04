f545b503975734d882327d3e9c9f5ebf0a8f551d
lcorreia <correialesp@gmail.com>
2020-12-03 23:34:18 -0300

    admin layout

--- admin/src/layouts/AdminLayout.vue
+++ admin/src/layouts/AdminLayout.vue
@@ -2,10 +2,17 @@
     <div id="page-top">
         <Loading :loading="loading" />
         <div id="wrapper">
-            <NavBar :toggled="sideBarToggled" v-on:toggled="toggleSideBar" v-on:logout="logout" />
+            <NavBar
+                :toggled="sideBarToggled"
+                v-on:toggled="toggleSideBar"
+                v-on:logout="logout" />
             <div id="content-wrapper" class="d-flex flex-column">
                 <div id="content">
-                    <NavBarTop v-on:sidebar-toggled="toggleSideBar" v-on:logout="logout" />
+                    <NavBarTop
+                        :userDropdownToggled="userDropdownToggled"
+                        v-on:sidebar-toggled="toggleSideBar"
+                        v-on:user-dropdown-toggled="toggleUserDropdown"
+                        v-on:logout="logout" />
                 </div>
             </div>
         </div>
@@ -27,8 +34,15 @@ export default {
     data() {
         return {
             loading: false,
-            sideBarToggled: false
+            sideBarToggled: false,
+            userDropdownToggled: false
         };
+    },
+    mounted() {
+        document.addEventListener('click', this.close);
+    },
+    beforeUnmount() {
+        document.removeEventListener('click', this.close);
     },
     methods: {
         async logout() {
@@ -42,6 +56,14 @@ export default {
         },
         toggleSideBar() {
             this.sideBarToggled = !this.sideBarToggled;
+        },
+        toggleUserDropdown() {
+            this.userDropdownToggled = !this.userDropdownToggled;
+        },
+        close(event) {
+            if (!document.getElementById('userDropdown').contains(event.target)) {
+                this.userDropdownToggled = false;
+            }
         }
     }
 }
